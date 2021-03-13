8c1064c50462b7fef0ec5b3a90e65fd33763ba93
rakeru2006 <rakeru2006@gmail.com>
2021-03-12 20:24:31 -0600

    filling the object to call AI

--- models/book.js
+++ models/book.js
@@ -2,10 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
+  _id: { type: String },
+  title: { type: String },
+  authors: { type: Array },
+  description: { type: String },
+  image: { type: String },
+  link: { type: String }
+  /*
   title: { type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
   date: { type: Date, default: Date.now }
+*/
+
+
+
 });
 
 const Book = mongoose.model("Book", bookSchema);

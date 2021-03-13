f11ed7843472ddb7d34cfc19062404299cc4240e
rakeru2006 <rakeru2006@gmail.com>
2021-03-13 00:13:40 -0600

    adding correct global variables to deploy mongo db in heroku

--- server.js
+++ server.js
@@ -16,8 +16,14 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookapigoogle");
 
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookapigoogle', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
